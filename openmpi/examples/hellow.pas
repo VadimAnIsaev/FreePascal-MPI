392d73e622c1ae4a2fffb8085640d12234a34e3c
VadimAnIsaev <vadim.an.isaev@gmail.com>
2019-10-07 22:20:54 +0700

    Update hellow.pas

--- openmpi/examples/hellow.pas
+++ openmpi/examples/hellow.pas
@@ -4,12 +4,10 @@ Uses mpi;
 Var
     rank: integer;
     size: integer;
-    MPI_COMM_F: integer;
 
 Begin
-    MPI_COMM_F:=0;
     MPI_Init(MPI_ARGC_NULL, NIL);
-    MPI_COMM_WORLD:=MPI_Comm_f2c(MPI_COMM_F);
+    MPI_COMM_WORLD:=MPI_Comm_f2c(MPI_COMM_WORLD_FORTRAN);
     MPI_Comm_rank(MPI_COMM_WORLD, @rank);
     MPI_Comm_size(MPI_COMM_WORLD, @size);
     Writeln('Hello world from process ', rank, ' of ', size);
