34cdf02574c9fa8895de8b6c20cdf05972cd8d2c
VadimAnIsaev <vadim.an.isaev@gmail.com>
2019-10-09 16:41:09 +0700

    Add files via upload

--- openmpi/examples/hellow.pas
+++ openmpi/examples/hellow.pas
@@ -1,19 +1,13 @@
-{$mode objfpc}
 Uses mpi;
 
 Var
     rank: integer;
     size: integer;
-    len: integer;
-    processor: array[0..MPI_MAX_PROCESSOR_NAME] of Char;
 
 Begin
-    MPI_Init(MPI_ARGC_NULL, NIL);
-    MPI_COMM_WORLD:=MPI_Comm_f2c(MPI_COMM_WORLD_FORTRAN);
+    MPI_Init2();
     MPI_Comm_rank(MPI_COMM_WORLD, @rank);
     MPI_Comm_size(MPI_COMM_WORLD, @size);
-    MPI_Get_Processor_Name(@processor[0], @len);
     Writeln('Hello world from process ', rank, ' of ', size);
-    WriteLn('I am running on host ', processor);
     MPI_Finalize();
 End.
