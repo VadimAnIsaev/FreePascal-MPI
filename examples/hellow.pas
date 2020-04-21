2b6046b1c906e9725d66a4ca3d5b500488629c2f
VadimAnIsaev <vadim.an.isaev@gmail.com>
2020-04-21 17:09:32 +0700

    Update hellow.pas

--- examples/hellow.pas
+++ examples/hellow.pas
@@ -5,11 +5,7 @@ Var
     size: integer;
 
 Begin
-{$IFDEF OpenMPI}    
-  MPI_Init2();
-{$ELSE}
-  MPI_Init(NIL, NIL);
-{$ENDIF}
+  MPI_Init();
   MPI_Comm_rank(MPI_COMM_WORLD, @rank);
   MPI_Comm_size(MPI_COMM_WORLD, @size);
   Writeln('Hello world from process ', rank, ' of ', size);
